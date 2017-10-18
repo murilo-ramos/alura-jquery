a19edd4ca5f2bc2a2fdc72c72a1dcc758be9e80a
Murilo Costa <murilo.costa@safrangroup.com>
2017-10-18 09:44:57 -0200

    Add time interval to disable input box when start typing

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,26 @@
+/*let frase = $(".frase").text();
+let numPalavras = frase.split(" ").length;
+
+let tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numPalavras);*/
+
 let campo = $(".campo-digitacao");
 
 campo.on("input", function () {
 	let conteudo = campo.val();
 	$("#contador-caracteres").text(conteudo.length);
 	$("#contador-palavras").text(conteudo.split(/\S+/).length - 1);
+});
+
+let tempoDigitacao = $("#tempo-digitacao");
+let tempoRestante  = tempoDigitacao.text();
+campo.one("focus", function () {
+	var cronometroId = setInterval(function () {
+		tempoRestante--;
+		tempoDigitacao.text(tempoRestante);
+		if (tempoRestante < 1) {
+			campo.attr("disabled", true);
+			clearInterval(cronometroId);
+		}
+	}, 1000);
 });
