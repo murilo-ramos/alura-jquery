709242279acecda6d9a351a53b6844a937c986de
Murilo Costa <murilo.costa@safrangroup.com>
2017-10-19 08:58:27 -0200

    Add validation of typed phrase, include styles using materialize, fonts, etc..

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,14 @@
-/*let frase = $(".frase").text();
-let numPalavras = frase.split(" ").length;
-
-let tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);*/
 
 let campo 		   = $(".campo-digitacao");
 let tempoDigitacao = $("#tempo-digitacao");
 let botaoReiniciar = $("#botao-reiniciar");
+let frase 		   = $(".frase").text();
 let tempoInicial   = tempoDigitacao.text();
 
 $(function() {
+	inicializaTamanhoFrase();
 	inicializaContadores();
+	inicializaMarcadores();
 	inicializaCronometro();
 	botaoReiniciar.click(reiniciaJogo);
 });
@@ -18,10 +16,18 @@ $(function() {
 function reiniciaJogo() {
 		campo.attr("disabled", false);
 		campo.val("");
+		campo.toggleClass("campo-desativado");
+		campo.removeClass("borda-verde");
+		campo.removeClass("borda-vermelha");
 		$("#contador-caracteres").text("0");
 		$("#contador-palavras").text("0");
 		$("#tempo-digitacao").text(tempoInicial);
 		inicializaCronometro();
+}
+
+function inicializaTamanhoFrase() {	
+	let numPalavras  = frase.split(" ").length;
+	let tamanhoFrase = $("#tamanho-frase").text(numPalavras);
 }
 
 function inicializaContadores() {
@@ -32,6 +38,19 @@ function inicializaContadores() {
 	});
 }
 
+function inicializaMarcadores() {
+	campo.on("input", function () {
+		let digitado   = campo.val();
+		if (frase.startsWith(digitado)) {
+			campo.addClass("borda-verde");
+			campo.removeClass("borda-vermelha");
+	    } else  {
+	    	campo.addClass("borda-vermelha");
+			campo.removeClass("borda-verde");
+		}
+	});
+}
+
 function inicializaCronometro() {
 	let tempoRestante  = tempoDigitacao.text();
 	campo.one("focus", function () {
@@ -39,8 +58,9 @@ function inicializaCronometro() {
 		var cronometroId = setInterval(function () {
 			tempoRestante--;
 			tempoDigitacao.text(tempoRestante);
-			if (tempoRestante < 1) {
+			if (tempoRestante < 1) {				
 				campo.attr("disabled", true);
+				campo.toggleClass("campo-desativado");
 				botaoReiniciar.attr("disabled", false);
 				clearInterval(cronometroId);
 			}
